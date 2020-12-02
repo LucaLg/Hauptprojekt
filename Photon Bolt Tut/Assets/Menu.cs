00d48d373b8b8454c3796402c84a54f084e3cab5
Philip Kalkoff <ftPhilip@Kalkoff.de>
2020-12-02 13:23:59 +0100

    Tutorial

---
+++ Photon Bolt Tut/Assets/Menu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Bolt;
+using Bolt.Matchmaking;
+using UdpKit;
+using System.Diagnostics;
+
+public class Menu : GlobalEventListener
+{
+    public void StartServer()
+    {
+        BoltLauncher.StartServer();
+    }
+ 
+    public override void BoltStartDone()
+    {
+        BoltMatchmaking.CreateSession("test", sceneToLoad: "Game");
+    }
+    public void StartClient()
+    {
+        BoltLauncher.StartClient();
+
+    }
+
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        foreach (var session in sessionList)
+        {
+            UdpSession photonSession = session.Value as UdpSession;
+
+            if (photonSession.Source == UdpSessionSource.Photon)
+            {
+                BoltMatchmaking.JoinSession(photonSession);
+            }
+        }
+    }
+}
---
+++ Photon Bolt Tut/Assets/NetworkCallbacks.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class NetworkCallbacks : GlobalEventListener
+{
+    public GameObject body;
+    public override void SceneLoadLocalDone(string scene)
+    {
+        Vector3 spawnPos = new Vector3(0, 0, 0);
+        BoltNetwork.Instantiate(body, spawnPos, Quaternion.identity);
+    }
+}
