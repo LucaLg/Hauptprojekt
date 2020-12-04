a2a0d24c8c929fa5607581c60f707d130167fe43
luca <lucal.gno@gmail.com>
2020-12-04 12:32:04 +0100

    CodeAngefangenBoden

---
+++ Photon Bolt Tut/Assets/MovePlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : Bolt.EntityBehaviour<ICustomSpielerState>
+{
+    private Vector2 movement;
+    //void Start()
+   public override void Attached(){
+      state.SetTransforms(state.spielerTransform,gameObject.transform);
+   }
+   public override void SimulateOwner(){
+       movement.y = Input.GetAxisRaw("Vertical");
+   }
+}
