7d0a32ea2bbc1e18f98152ee9f967bbd53f32466
luca <lucal.gno@gmail.com>
2020-12-08 16:36:36 +0100

    HauptprojektUpdate

---
+++ HauptprojektJNR/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class EnemyController : MonoBehaviour,IPunObservable
+{
+    public int health = 4;
+    private PhotonView photonView;
+    bool isDead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+        photonView.RPC("Die", RpcTarget.All);
+        if (isDead)
+        {
+            if (photonView.IsMine) { 
+            PhotonNetwork.Destroy(gameObject);
+            }
+        }
+    }
+    [PunRPC]
+    void Die()
+    {
+        if (health ==0)
+        {
+           
+            isDead = true;
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        
+        if (stream.IsWriting)
+        {
+            stream.SendNext(health);
+        }else if (stream.IsReading)
+        {
+            health = (int)stream.ReceiveNext();
+        }
+    }
+}
---
+++ HauptprojektJNR/Assets/Scripts/PhotonScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonScript : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+        PhotonNetwork.JoinOrCreateRoom("Room1", roomOptions, null);
+    }
+    public override void OnJoinedRoom()
+    {
+        float x = Random.Range(-3, 5);
+        PhotonNetwork.Instantiate("Player", new Vector3(0, -3, 0), Quaternion.identity);
+        //PhotonNetwork.Instantiate("Enemy", new Vector3(x, -1.57f, 0), Quaternion.identity);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
