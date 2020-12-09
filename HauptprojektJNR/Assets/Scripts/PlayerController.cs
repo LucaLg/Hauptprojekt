6e84f97ad460e6ef2528702f58f5d5bc80b2dabd
Philip Kalkoff <ftPhilip@Kalkoff.de>
2020-12-09 23:06:54 +0100

    Bessere Animationen, PlayerHealth Leiste

--- HauptprojektJNR/Assets/Scripts/PlayerController.cs
+++ HauptprojektJNR/Assets/Scripts/PlayerController.cs
@@ -15,17 +15,22 @@ public class PlayerController : MonoBehaviourPun
     SpriteRenderer spriteR;
     private BoxCollider2D playerBox;
     private Transform attackPoint;
+    private Transform healthBar;
 
     //Attribute
     private Vector2 movement;
     private float moveSpeed = 2f;
     private float jumpForce = 5f;
     public float attackRange = 1f;
+    public float maxHealth = 10f;
+    public float health;
     
 
     
     void Start()
     {
+        health = maxHealth;
+        healthBar = transform.Find("HealthBar/Bar");
         animPlayer = GetComponent<Animator>();
         animPlayer.SetBool("Grounded", true);
         playerRb = GetComponent<Rigidbody2D>();
@@ -38,21 +43,27 @@ public class PlayerController : MonoBehaviourPun
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
         if (!photonView.IsMine) return;
+
+        //Movement
+        movement.x = Input.GetAxisRaw("Horizontal");
         if(Input.GetAxis("Horizontal") != 0) {
+            playerRb.MovePosition(playerRb.position + movement * moveSpeed * Time.fixedDeltaTime);
             
-           playerRb.MovePosition(playerRb.position + movement * moveSpeed * Time.fixedDeltaTime);
-            animPlayer.SetInteger("AnimState", 1);
+            animPlayer.SetBool("isMoving", true);
         }
         else
         {
-            animPlayer.SetInteger("AnimState", 0);
+            animPlayer.SetBool("isMoving", false);
         }
-        if (Input.GetKeyDown("space") && isGrounded()){
+        bool grounded = isGrounded();
+        if (Input.GetKeyDown("space") && grounded){
+            Debug.Log("Jump");
+            animPlayer.SetTrigger("Jump");
             playerRb.AddForce(new Vector2(0, 7), ForceMode2D.Impulse);
             playerRb.velocity = Vector2.up * jumpForce;
         }
+        
         if (Input.GetKeyDown(KeyCode.A))
         {
             photonView.RPC("FlipTrue", RpcTarget.AllBuffered);
@@ -63,28 +74,34 @@ public class PlayerController : MonoBehaviourPun
         }
         if (Input.GetKeyDown("b"))
         {
+            Debug.Log("Attack pressed");
             photonView.RPC("Attack", RpcTarget.AllBuffered);
         }
+
+        //Update Health (Ergibt 0 - Warum??)
+        float healthPercentage = health / maxHealth;
+        Debug.Log(healthPercentage);
+        healthBar.localScale = new Vector3(healthPercentage, 1, 1);
     }
     private bool isGrounded()
     {
-       RaycastHit2D raycastHit2D =  Physics2D.BoxCast(playerBox.bounds.center, playerBox.bounds.size, 0f, Vector2.down,1f, platformLayer);
-        Debug.Log(raycastHit2D.collider != null);
-        return (raycastHit2D.collider != null);
+        RaycastHit2D raycastHit2D =  Physics2D.BoxCast(playerBox.bounds.center, playerBox.bounds.size, 0f, Vector2.down,0.1f, platformLayer);
+        bool grounded = raycastHit2D.collider != null;
+        Debug.Log(grounded);
+        animPlayer.SetBool("Grounded", grounded);
+        return (grounded);
     }
     [PunRPC]
     private void Attack()
     {
         //Play Animation
         animPlayer.SetTrigger("Attack");
-
+        Debug.Log("Attack entered");
         //Detect Enemies in range of Attack
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
         //Damage
         foreach (BoxCollider2D enemy in hitEnemies)
         {
-            
-            Debug.Log("Damage");
             enemy.GetComponentInParent<EnemyController>().health--;
 
         }
