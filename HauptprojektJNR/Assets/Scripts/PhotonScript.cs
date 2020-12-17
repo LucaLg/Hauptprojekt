4c8fe59456cf8bea3a9a36150551daeb797af0d2
luca <lucal.gno@gmail.com>
2020-12-17 13:46:33 +0100

    MapAusgebaut

--- HauptprojektJNR/Assets/Scripts/PhotonScript.cs
+++ HauptprojektJNR/Assets/Scripts/PhotonScript.cs
@@ -6,6 +6,7 @@ using Photon.Realtime;
 
 public class PhotonScript : MonoBehaviourPunCallbacks
 {
+    public Transform spawnPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +21,8 @@ public class PhotonScript : MonoBehaviourPunCallbacks
     }
     public override void OnJoinedRoom()
     {
-        float x = Random.Range(-3, 5);
-        PhotonNetwork.Instantiate("Player", new Vector3(0, -3, 0), Quaternion.identity);
+        //float x = Random.Range(-3, 5);
+        PhotonNetwork.Instantiate("Player", spawnPoint.position, Quaternion.identity);
         //PhotonNetwork.Instantiate("Enemy", new Vector3(x, -1.57f, 0), Quaternion.identity);
     }
     // Update is called once per frame
